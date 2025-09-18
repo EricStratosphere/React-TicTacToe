0bcd9b2e4edbd23f0d6b5943aa44ded87c381fca
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2025-09-18 17:49:29 +0800

    Add logic for conditional rendering of Cell content on click.

--- react-tictactoe/src/components/Grid.tsx
+++ react-tictactoe/src/components/Grid.tsx
@@ -9,29 +9,38 @@ const ticTacToe : string[][] = [
 ]
 
 export default function Grid(){
-    const x : string = "";
-    const [textContent, setTextContent] = useState(x);
+
+    const s : boolean = true;
+
     const [matrix, setMatrix] = useState(ticTacToe);
     
+    const [xTurn, setXTurn] = useState(s);
+
     return(
+        <>
+        <h2>
+            {xTurn ? "It is X's Turn" : "It is O's Turn"}
+        </h2>
         <div className="outer-grid">
             <div className='cell-container'>
                 <div className='row 0'>
-                    <Cell row = {0} column={0} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {0} column={1} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {0} column={2} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {0} column={0} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {0} column={1} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {0} column={2} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
                 </div>
                 <div className='row 1'>
-                    <Cell row = {1} column={0} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {1} column={1} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {1} column={2} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {1} column={0} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {1} column={1} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {1} column={2} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
                 </div>
                 <div className='row 2'>
-                    <Cell row = {2} column={0} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {2} column={1} textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell row = {2} column={2} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {2} column={0} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {2} column={1} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
+                    <Cell row = {2} column={2} xTurn={xTurn} setXTurn={setXTurn} matrix={matrix} setMatrix={setMatrix}/>
                 </div>
             </div>
         </div>
+        </>
+
     );
 }
--- react-tictactoe/src/components/cell.tsx
+++ react-tictactoe/src/components/cell.tsx
@@ -1,16 +1,30 @@
 import '../styles/grid.css'
+import {useState} from 'react';
 interface CellProps{
     row : number;
     column : number;
-    textContent : string;
-    setTextContent : (text : string) => void;
+    matrix : string[][];
+    xTurn : boolean;
+    setXTurn : (xTurn : boolean) => void;
+    setMatrix : (matrix : string[][]) => void;
 }
 
-const Cell = ({row, column, textContent, setTextContent} : CellProps) => {
-
+const Cell = ({row, column, xTurn, setXTurn, matrix, setMatrix} : CellProps) => {
+    const x : string = ""
+    const [textContent, setTextContent] = useState(x);
     return(
-    <div className="cell" onClick={() => {
-        setTextContent(textContent)
+    <div className="cell" onClick={() : void => {
+        if(textContent){
+            alert("This cell is already taken!");
+            return;
+        }
+        const tempContent : string = xTurn ? "X" : "O";
+        setTextContent(tempContent);
+        setXTurn(!xTurn);
+        const newMatrix : string[][] = JSON.parse(JSON.stringify(matrix));
+        newMatrix[row][column] = xTurn ? "X" : "O";
+        console.log(newMatrix);
+        setMatrix(newMatrix);
     }
     }>
         {textContent}
