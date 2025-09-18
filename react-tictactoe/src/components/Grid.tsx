3b50fef28887d6b20ffc046595e78db5f692eb1c
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2025-09-18 16:55:10 +0800

    Reformatted cell structure

--- react-tictactoe/src/components/Grid.tsx
+++ react-tictactoe/src/components/Grid.tsx
@@ -16,20 +16,20 @@ export default function Grid(){
     return(
         <div className="outer-grid">
             <div className='cell-container'>
-                <div className='row'>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
+                <div className='row 0'>
+                    <Cell row = {0} column={0} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {0} column={1} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {0} column={2} textContent={textContent} setTextContent={setTextContent}/>
                 </div>
-                <div className='row'>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
+                <div className='row 1'>
+                    <Cell row = {1} column={0} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {1} column={1} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {1} column={2} textContent={textContent} setTextContent={setTextContent}/>
                 </div>
-                <div className='row'>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
-                    <Cell textContent={textContent} setTextContent={setTextContent}/>
+                <div className='row 2'>
+                    <Cell row = {2} column={0} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {2} column={1} textContent={textContent} setTextContent={setTextContent}/>
+                    <Cell row = {2} column={2} textContent={textContent} setTextContent={setTextContent}/>
                 </div>
             </div>
         </div>
